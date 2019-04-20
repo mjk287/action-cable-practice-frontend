5ac921dedcacfe85d429f51d9b7e875c92b74b28
github username <github email address>
2019-04-19 21:59:20 -0400

    set up redux and routes. redux only keeps track of current user. implemented login signup form in landing page

---
+++ src/components/Signup.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+class Signup extends React.Component{
+  state = {
+    username: '',
+    password: ''
+  }
+
+  changeHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render(){
+    return(
+      <form>
+        <label>
+        Username:
+        <input type='text' name='username' value={this.state.username} onChange={this.changeHandler}/>
+        </label>
+        <label>
+        Password:
+        <input type='text' name='password' value={this.state.password} onChange={this.changeHandler}/>
+        </label>
+        <input type='submit' value='Submit Me!'/>
+      </form>
+    )
+  }
+}
+
+export default Signup
